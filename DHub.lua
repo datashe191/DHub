293412bcade6bf9fb6de22e825b9029c1b1b19bf
datashe191 <shekiladzedat2015@gmail.com>
2025-07-15 21:07:40 +0400

    Update DHub.lua

--- DHub.lua
+++ DHub.lua
@@ -9,10 +9,20 @@ local Window = Rayfield:CreateWindow({
     ConfigurationSaving = {
         Enabled = false
     },
-    Discord = {
-        Enabled = false
+Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/kWUsAj98", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = false
+    KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"FX9R-V3LZ-7QW2-MP6D-GYJX-UBN4-KCEA"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 })
 
 -- RARE Seeds for dropdown
